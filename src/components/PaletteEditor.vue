7bde2774801b0d0bcb8c39e9a342e8db8cea02cc
Max Zakharzhevskiy <max.za@digitalpersona.com>
2020-10-17 20:02:44 -0700

    Create stop pins

--- src/components/PaletteEditor.vue
+++ src/components/PaletteEditor.vue
@@ -6,43 +6,59 @@
             class="palette-svg"
         >
             <defs>
-                <!-- <linearGradient id="pal">
-                    <stop offset="0%" stop-color="red" />
-                    <stop offset="100%" stop-color="gold" />
-                </linearGradient> -->
-
                 <linearGradient id="pal">
-                    <stop v-for="(stripe, index) of stripes" :key="index" :offset="stripe.val" :stop-color="stripe.color"/>
+                    <stop
+                        v-for="(stripe, index) of currentStripes"
+                        :key="index"
+                        :offset="`${stripe.val}%`"
+                        :stop-color="stripe.color"
+                    />
                 </linearGradient>
             </defs>
+
+            <template
+                v-for="(stripe, index) of currentStripes"
+                :key="index"
+            >
+                <circle :cx="`${+stripe.val + 5}%`" cy="20" r="5%" />
+                <rect :x="`${stripe.val}%`" y="0" width="2" height="40" fill="red" />
+            </template>
+
             <rect x="5%" y="50%" width="90%" height="40%" fill="url(#pal)" />
         </svg>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, ref } from "vue";
+
+type ColorStop = {
+    val: string;
+    color: string;
+};
+
+const defaultPalette = [
+    {
+        val: "0",
+        color: "red",
+    },
+    {
+        val: "100",
+        color: "gold",
+    },
+];
 
 export default defineComponent({
     setup() {
-        type ColorStop = {
-            val: string;
-            color: string;
-        };
+        let stripes = ref<ColorStop[]>([]);
 
-        let stripes = ref<ColorStop[]>([
-            {
-                val: '0%',
-                color: 'red',
-            },
-            {
-                val: '100%',
-                color: 'gold',
-            },
-        ]);
+        const currentStripes = computed(() => {
+            return stripes.value.length ? stripes.value : defaultPalette;
+        });
 
         return {
             stripes,
+            currentStripes,
         };
     },
 });
