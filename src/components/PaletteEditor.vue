593fa433c312decfa39258ee8bd950bdcf4f0f8d
Max Zakharzhevskiy <max.za@digitalpersona.com>
2020-10-17 21:23:36 -0700

    Add const enums

--- src/components/PaletteEditor.vue
+++ src/components/PaletteEditor.vue
@@ -20,8 +20,8 @@
                 v-for="(stripe, index) of currentStripes"
                 :key="index"
             >
-                <circle :cx="posCircle(stripe.val)" cy="20" r="5%" />
-                <rect :x="`${posPin(stripe.val)}`" y="0" width="1%" height="40" fill="red" />
+                <circle :cx="posCircle(stripe.val)" cy="20" r="5%" fill="gray" stroke-width=1 stroke="red" />
+                <rect :x="`${posPin(stripe.val)}`" y="20" width="1%" height="40" fill="red" />
             </template>
 
             <rect x="5%" y="50%" width="90%" height="40%" fill="url(#pal)" />
@@ -37,6 +37,15 @@ type ColorStop = {
     color: string;
 };
 
+const enum SIZES {
+    pinWidth = 1,
+    pinWidthHalf = 0.5,
+    left = 5,
+    leftCircle = left + pinWidth / 2,
+    right = 5,
+    newRange = 100 - left - right - pinWidth,
+}
+
 const defaultPalette = [
     {
         val: "0",
@@ -45,6 +54,14 @@ const defaultPalette = [
     {
         val: "50",
         color: "blue",
+    },
+    {
+        val: "60",
+        color: "blue",
+    },
+    {
+        val: "60",
+        color: "green",
     },
     {
         val: "100",
@@ -60,15 +77,14 @@ export default defineComponent({
             return stripes.value.length ? stripes.value : defaultPalette;
         });
 
+        function pos(val: string | number): number {
+            return +val * SIZES.newRange / 100; // val * (<new range=89> = 100 - <left=5> - <right=5> - <pin width=1>) / <old range=100>
+        }
         function posCircle(val: string | number): string {
-            let curr = +val;
-            curr = 5.5 + curr * (89) / 100; // val * (<new range=89> = 100 - <left=5> - <right=5> - <pin width=1>) / <old range=100> + <left=5> + <pin width=1> / 2
-            return `${curr}%`;
+            return `${SIZES.leftCircle + pos(val)}%`; // val * (<new range=89> = 100 - <left=5> - <right=5> - <pin width=1>) / <old range=100> + <left=5> + <pin width=1> / 2
         }
         function posPin(val: string | number): string {
-            let curr = +val;
-            curr = 5 + curr * 89 / 100; // val * (<new range=89> = 100 - <left=5> - <right=5> - <pin width=1>) / <old range=100> + <left=5>
-            return `${curr}%`;
+            return `${SIZES.left + pos(val)}%`; // val * (<new range=89> = 100 - <left=5> - <right=5> - <pin width=1>) / <old range=100> + <left=5>
         }
 
         return {
