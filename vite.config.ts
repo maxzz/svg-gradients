c5498681f8086c27470e7701490190f18fa11193
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-20 19:29:27 -0700

    Feature: add new remote name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default (command) => {
     return defineConfig({
-        base: command === 'build' ? '/gradients/' : '',
+        base: command === 'build' ? '/svg-gradients/' : '',
         plugins: [vue()],
     });
 }
